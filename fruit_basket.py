cb4bfc9d96156769cd564a7fe30b1101e4b9b05e
kkolon <32082328+kkolon@users.noreply.github.com>
2017-09-26 15:55:09 -0500

    Create fruit_basket.py

---
+++ fruit_basket.py
@@ -0,0 +1,11 @@
+#Fruit Basket Game
+
+fruit = 'grapefruit', 'pear', 'cherry', 'pineapple', or 'apple'
+
+user_choice = input('Pick a fruit for my fruit basket')
+
+if user_choice == fruit
+    print('Yay! You are correct!')
+else:
+   print('Not my kind of fruit')
+  
