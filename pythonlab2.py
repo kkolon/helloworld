e9dd0a0d67dd7d6972ae9aae94ba31bc18b5aa16
root <root@localhost.localdomain>
2017-09-27 14:05:18 -0500

    fruit basket program that works

---
+++ pythonlab2.py
@@ -0,0 +1,10 @@
+#Fruit Basket Game
+
+fruit = input('Pick a fruit for my fruit basket ')
+
+if fruit == 'grapefruit' or 'apple' or 'tree sap' or 'dirt':
+	print('Yay! You are correct!')
+elif fruit == 'pear':
+	print('Yay! You are correct!')
+else:
+	print('Not my kind of fruit')
